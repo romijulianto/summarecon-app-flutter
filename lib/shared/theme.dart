4c8382146db04432c8fd78bcc885c54a662903ea
Romi Julianto <romyjulians@gmail.com>
2022-10-18 00:33:37 +0700

    add style guide theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color Style
+Color kPrimaryColor = Color(0xEE6233);
+Color kBlackColor = Color(0x000000);
+Color kWhiteColor = Color(0xFFFFFF);
+Color kBackgroundColor = Color(0xFFFFFF);
+Color kSecondaryColor = Color(0xEE6233);
+
+// Text Style
+TextStyle blackTextStyle = GoogleFonts.inriaSerif(
+  color: kBlackColor,
+);
