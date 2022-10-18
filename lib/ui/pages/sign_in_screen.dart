1a0204d280b1fa23d09549f21733f7baeb5ac955
Romi Julianto <romyjulians@gmail.com>
2022-10-18 11:06:32 +0700

    sign_in screnn finish

--- lib/ui/pages/sign_in_screen.dart
+++ lib/ui/pages/sign_in_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:summareconapps/shared/theme.dart';
+import '../../shared/theme.dart';
 // ignore_for_file: prefer_const_constructors
 
 class SignIn extends StatelessWidget {
@@ -8,6 +8,178 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Widget Input Section SignIn
+    Widget inputSection() {
+      Widget emailInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Email',
+                style: GoogleFonts.inriaSerif(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget passwordInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 33),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Password',
+                style: GoogleFonts.inriaSerif(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                obscureText: true,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget loginButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.orangeAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  defaultRadius,
+                ),
+              ),
+            ),
+            child: Text(
+              'Login',
+              style: GoogleFonts.inriaSerif(
+                color: Colors.white,
+                fontSize: 20.33,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        );
+      }
+
+      Widget forgotPassword() {
+        return Container(
+          margin: EdgeInsets.only(
+            top: 5,
+            bottom: 5,
+          ),
+          child: Column(
+            children: [
+              Text(
+                'Forgot your password?',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.inriaSerif(
+                  color: Colors.black,
+                  fontSize: 16.94,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Don’t have an account?',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.inriaSerif(
+                  color: Colors.black,
+                  fontSize: 16.94,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget signUpButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.orangeAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  defaultRadius,
+                ),
+              ),
+            ),
+            child: Text(
+              'Sign Up',
+              style: GoogleFonts.inriaSerif(
+                color: Colors.white,
+                fontSize: 20.33,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        );
+      }
+
+      return Container(
+        margin: EdgeInsets.only(top: 14),
+        padding: EdgeInsets.symmetric(
+          horizontal: 40,
+        ),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(
+            defaultRadius,
+          ),
+        ),
+        child: Column(children: [
+          emailInput(),
+          passwordInput(),
+          loginButton(),
+          forgotPassword(),
+          signUpButton(),
+        ]),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kWhiteColor,
       body: Column(
@@ -29,7 +201,8 @@ class SignIn extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-          )
+          ),
+          inputSection(),
         ],
       ),
     );
